f19e78300b4787579a3f1076e03295182b39c02f
jeberst <j_eberst@yahoo.com>
2014-10-05 20:14:06 -0400

    added the project for real this time.

---
+++ NodeXL/DistributedBreadthFirstSearch/GraphGenerator.cs
@@ -0,0 +1,53 @@
+﻿using Smrf.NodeXL.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smrf.NodeXL.Layouts;
+using Smrf.NodeXL.Algorithms;
+using Smrf.NodeXL.GraphDataProviders.Facebook;
+using System.Threading;
+using System.IO;
+using System.Xml;
+using Smrf.NodeXL.Adapters;
+
+namespace DistributedBreadthFirstSearch
+{
+    public class GraphGenerator
+    {
+        public GraphGenerator() { }
+
+        public IGraph Generator()
+        {
+            writeFile();
+
+            GraphMLGraphAdapter graphMlAdapter = new Smrf.NodeXL.Adapters.GraphMLGraphAdapter();
+            IGraph graph = graphMlAdapter.LoadGraphFromFile("GraphAsGraphML.txt");
+            return graph;
+        }
+
+        private void writeFile()
+        {
+            Smrf.NodeXL.GraphDataProviders.Facebook.FacebookGraphDataProvider fbGraph = new Smrf.NodeXL.GraphDataProviders.Facebook.FacebookGraphDataProvider();
+            string data = "";
+
+            try
+            {
+                fbGraph.TryGetGraphData(out data);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n Please copy the following information and paste it to http://socialnetimporter.codeplex.com/discussions :\n" + e.StackTrace);
+            }
+            TextWriter tw = new StreamWriter("GraphASGraphML.txt");
+
+
+            // write a line of text to the file
+            tw.WriteLine(data);
+
+            // close the stream
+            tw.Close();
+        }
+    }
+}
---
+++ NodeXL/DistributedBreadthFirstSearch/Program.cs
@@ -0,0 +1,73 @@
+﻿using Smrf.NodeXL.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedBreadthFirstSearch
+{
+    class Program
+    {
+         [STAThread]
+        static void Main(string[] args)
+        {
+            GraphGenerator graphGenerator = new GraphGenerator();
+            IGraph graph = graphGenerator.Generator();
+
+            String[] metadataCollection = graph.GetRequiredValue(ReservedMetadataKeys.AllVertexMetadataKeys, typeof(String[])) as String[];
+            List<string> vertexExampleValues = new List<string>();
+
+            foreach (var metadata in metadataCollection)
+            {
+                IVertex vertex = graph.Vertices.FirstOrDefault();
+                if (vertex.GetValue(metadata) != null)
+                {
+                    vertexExampleValues.Add(vertex.GetValue(metadata).ToString());
+                }
+            }
+
+            var ShowTheExamples = vertexExampleValues;
+
+            IVertex discoveredVertex = BreadthFirstSearch(graph, "Last Name", "Pecoraro");
+            if (discoveredVertex != null)
+            {
+                string firstName = discoveredVertex.GetValue("First Name").ToString();
+                string lastName = discoveredVertex.GetValue("Last Name").ToString();
+            }
+        }
+
+         static IVertex BreadthFirstSearch(IGraph graph, string searchKey, string searchValue)
+         {
+             Queue<IVertex> queue = new Queue<IVertex>();
+             IVertex root = graph.Vertices.FirstOrDefault();
+             Dictionary<IVertex, bool> visited = new Dictionary<IVertex, bool>();
+             
+             queue.Enqueue(root);
+             visited[root] = true;
+
+             while (queue.Count > 0)
+             {
+                 IVertex currentNode = queue.Dequeue();
+
+                 string val = currentNode.GetValue(searchKey).ToString();
+                 if (currentNode.GetValue(searchKey).ToString() == searchValue)
+                 {
+                     return currentNode;
+                 }
+
+                 foreach (IEdge edge in currentNode.IncidentEdges)
+                 {
+                     IVertex child = edge.Vertex2;
+                     if (!visited.ContainsKey(child))
+                     {
+                         visited[child] = true;
+                         queue.Enqueue(child);
+                     }
+                 }
+             }
+
+             return null;
+         }
+    }
+}
